3199d8f4b374eff521f4560e19c6985926a0fa37
DAVIDMWELU <davidmwelu@gmail.com>
2025-01-27 06:03:20 +0300

    Implement constructor functions and set up tests

---
+++ index.js
@@ -0,0 +1,20 @@
+// Scooter Constructor
+function Scooter(year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
+  
+  // Driver Constructor
+  function Driver(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+  
+  // PickupLocation Constructor
+  function PickupLocation(address, city) {
+    this.address = address;
+    this.city = city;
+  }
+  
